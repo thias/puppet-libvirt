b2a6a5134e49c3e1fbac82a376c6d3dcbd7e0e53
Igor Galić <i.galic@brainsware.org>
2013-10-09 11:30:25 +0200

    put all non-boolean parameters in params.pp
    let's just go all the way and put all of those parameters into params.
    We now also set the correct group and the correct group permissions on
    Debuntu. Btw, does anyone notice how on RHEL the package is libvirt, the
    group is libvirt, and the service is unsurprisingly libvirtd, whereas on
    Debian they are all stupid?

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,21 +9,36 @@ class libvirt::params {
       $libvirt_package = "libvirt.${::architecture}"
       $libvirt_service = 'libvirtd'
       $virtinst_package = 'python-virtinst'
+
+      $unix_sock_dir = '/var/run/libvirt'
+      $unix_sock_group = 'root'
+      $unix_sock_ro_perms = '0777'
       $auth_unix_ro = undef
+      $unix_sock_rw_perms = '0700'
       $auth_unix_rw = undef
     }
     'Debian': {
       $libvirt_package = 'libvirt-bin'
       $libvirt_service = 'libvirt-bin'
       $virtinst_package = 'virtinst'
+
+      $unix_sock_dir = '/var/run/libvirt'
+      $unix_sock_group = 'libvirtd'
       $auth_unix_ro = 'none'
+      $unix_sock_rw_perms = '0770'
       $auth_unix_rw = 'none'
     }
     default: {
       $libvirt_package = 'libvirt'
       $libvirt_service = 'libvirtd'
       $virtinst_package = 'python-virtinst'
+
+      $unix_sock_dir = '/var/run/libvirt'
+      $unix_sock_group = 'root'
+      $unix_sock_ro_perms = '0777'
       $auth_unix_ro = undef
+      $unix_sock_rw_perms = '0700'
+      $auth_unix_rw = undef
     }
   }
 
