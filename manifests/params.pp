23addd31bffa23f8d6636ccd67b17d8295a9cea5
Marco Wessel <marco@poop.nl>
2015-03-05 16:37:19 +0100

    Wheezy backports and Jessie+ have different names for things

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,20 +18,33 @@ class libvirt::params {
       $deb_default = false
     }
     'Debian': {
-      $libvirt_package = 'libvirt-bin'
-      $libvirt_service = 'libvirt-bin'
-      $virtinst_package = 'virtinst'
-      $radvd_package = 'radvd'
-      $sysconfig = false
-      $deb_default = {}
-      # UNIX socket
-      $unix_sock_group = $::operatingsystem ? {
-	'Debian' => 'libvirt',
-	'Ubuntu' => 'libvirtd', 
-      }
-      $auth_unix_ro = 'none'
-      $unix_sock_rw_perms = '0770'
-      $auth_unix_rw = 'none'
+      case $::operatingsystem {
+        'Debian': {
+          $libvirt_package = 'libvirt-bin'
+          $libvirt_service = 'libvirtd'
+          $virtinst_package = 'virtinst'
+          $radvd_package = 'radvd'
+          $sysconfig = false
+          $deb_default = {}
+          # UNIX socket
+          $unix_sock_group = 'libvirt',
+          $auth_unix_ro = 'none'
+          $unix_sock_rw_perms = '0770'
+          $auth_unix_rw = 'none'
+        }
+      'Ubuntu': {
+          $libvirt_package = 'libvirt-bin'
+          $libvirt_service = 'libvirt-bin'
+          $virtinst_package = 'virtinst'
+          $radvd_package = 'radvd'
+          $sysconfig = false
+          $deb_default = {}
+          # UNIX socket
+          $unix_sock_group = 'libvirtd',
+          $auth_unix_ro = 'none'
+          $unix_sock_rw_perms = '0770'
+          $auth_unix_rw = 'none'
+        }
     }
     default: {
       $libvirt_package = 'libvirt'
