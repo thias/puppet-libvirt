cc815b5e52327ef7ea054d6fdf36f8c217030bfe
james <jaggededgedjustice@gmail.com>
2015-04-19 16:30:02 +0100

    more parsing and use resource in template

--- lib/puppet/provider/network/libvirt.rb
+++ lib/puppet/provider/network/libvirt.rb
@@ -42,6 +42,15 @@ Puppet::Type.type(:network).provide(:libvirt) do
             data[setting] = ip.attribute(setting).content
           end
         end
+        if ip.at_xpath('//dhcp')
+          data[:dhcp] = {}
+          for setting in ['start', 'end']
+            data[:dhcp][setting] = ip.at_xpath('//dhcp/range').attribute(setting).content
+          end
+          if ip.at_xpath('//dhcp/bootp')
+            data[:dhcp][:bootp_file] = ip.at_xpath('//dhcp/bootp').attribute('file').content
+          end
+        end
         if ip.attribute('family') and ip.attribute('family').content == 'ipv6'
           definition[:ipv6].push(data)
         else
@@ -87,20 +96,20 @@ net_xml = <<EOF
   <% if @property_hash[:mac] %>
   <mac address='<%= @property_hash[:mac] %>'/>
   <% end %>
-  <% if @property_hash[:forward_mode] %>
-  <forward<% if @property_hash[:forward_dev] %> dev='<%= @property_hash[:forward_dev] %>'<%end%> mode='<%= @property_hash[:forward_mode] %>'<% if !@property_hash[:forward_interfaces] %>/<%end%>>
-  <%  if @property_hash[:forward_interfaces] %>
-  <%    @property_hash[:forward_interfaces].each do |dev| %>
+  <% if @resource[:forward_mode] %>
+  <forward<% if @resource[:forward_dev] %> dev='<%= @resource[:forward_dev] %>'<%end%> mode='<%= @resource[:forward_mode] %>'<% if !@resource[:forward_interfaces] %>/<%end%>>
+  <%  if @resource[:forward_interfaces] %>
+  <%    @resource[:forward_interfaces].each do |dev| %>
     <interface dev='<%= dev %>'/>
   <%    end %>
   </forward>
   <%  end %>
   <% end %>
-  <% if @property_hash[:bridge] %>
-  <bridge name='<%= @property_hash[:bridge] %>'<% if @property_hash[:forward_mode] and @property_hash[:forward_mode] != 'bridge' %> stp='on' delay='0'<%end%>/>
+  <% if @resource[:bridge] %>
+  <bridge name='<%= @resource[:bridge] %>'<% if @resource[:forward_mode] and @resource[:forward_mode] != 'bridge' %> stp='on' delay='0'<%end%>/>
   <% end %>
-  <%if @property_hash[:ip] %>
-  <%  @property_hash[:ip].each do |ip| %>
+  <%if @resource[:ip] %>
+  <%  @resource[:ip].each do |ip| %>
   <ip<%if ip['address']%> address='<%=ip['address']%>'<%end%><% if ip['netmask']%> netmask='<%=ip['netmask']%>'<%end%><% if ip['prefix']%> prefix='<%=ip['prefix']%>'<%end%><% unless ip['dhcp'] %>/<% end %>>
     <% if ip['dhcp'] %>
     <% dhcp = ip['dhcp'] %>
@@ -116,8 +125,8 @@ net_xml = <<EOF
     <% end%>
   <%  end%>
   <%end%>
-  <% if @property_hash[:ipv6] %>
-  <%  @property_hash[:ipv6].each do |ip| %>
+  <% if @resource[:ipv6] %>
+  <%  @resource[:ipv6].each do |ip| %>
   <ip family='ipv6'<% if ip['address']%> address='<%=ip['address']%>'<%end%><% if ip['netmask']%> netmask='<%=ip['netmask']%>'<%end%><% if ip['prefix']%> prefix='<%=ip['prefix']%>'<%end%><% unless ip['dhcp'] %>/<% end %>>
     <% if ip['dhcp'] %>
     <% dhcp = ip['dhcp'] %>
